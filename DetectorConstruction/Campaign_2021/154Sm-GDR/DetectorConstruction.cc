82cce3a4902a8634a34f91145560104a1457c47a
RMolaeng <1511527@students.wits.ac.za>
2024-04-09 15:29:30 +0200

    hello

--- DetectorConstruction/Campaign_2021/154Sm-GDR/DetectorConstruction.cc
+++ DetectorConstruction/Campaign_2021/154Sm-GDR/DetectorConstruction.cc
@@ -83,32 +83,33 @@ const auto filterPbThinThickness = 1.158 * mm; // Aka 1mm Pb filter
 const auto filterPbThickThickness = 2.562 * mm; // Aka 2.5mm Pb filter
 
 array<DetectorPosition, 4> labr_positions{
-    DetectorPosition{"labr_L1_Sci_01", 90. * degree, 0. * degree, 1. * inch + 52.0 * mm, 0, filterCuThickThickness, 2 * filterPbThickThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"labr_L3_Sci_02", 90. * degree, 90. * degree, 1. * inch + 45.0 * mm, 0, filterCuThickThickness, 2 * filterPbThickThickness, G4ThreeVector(-4. * mm, 0, 0)},
-    DetectorPosition{"labr_L5_Sci_03", 90. * degree, 180. * degree, 1. * inch + 38.0 * mm, 0, filterCuThickThickness, 2 * filterPbThickThickness, G4ThreeVector(0, -4. * mm, 0)},
-    DetectorPosition{"labr_L7_Sci_04", 90. * degree, 270. * degree, 1. * inch + 40.0 * mm, 0, 2 * filterCuThickThickness, 2 * filterPbThickThickness, G4ThreeVector(0, 0, 0)},
+    DetectorPosition{"labr_L1_Sci_01", 135. * degree, 218.57 * degree, 8.7 * cm, 0, filterCuThickThickness, 2 * filterPbThickThickness, G4ThreeVector(0, 0, 0)},
+    DetectorPosition{"labr_L3_Sci_02", 135. * degree, 321.43 * degree, 8.7 * cm, 0, filterCuThickThickness, 2 * filterPbThickThickness, G4ThreeVector(-4. * mm, 0, 0)},
+    DetectorPosition{"labr_L5_Sci_03", 90. * degree, 180. * degree, 5.0 * cm , 0, filterCuThickThickness, 2 * filterPbThickThickness, G4ThreeVector(0, -4. * mm, 0)},
+    DetectorPosition{"labr_L7_Sci_04", 90. * degree, 270. * degree, 7.5 * cm, 0, 2 * filterCuThickThickness, 2 * filterPbThickThickness, G4ThreeVector(0, 0, 0)},
 };
 
-array<DetectorPosition, 6> clover_positions{
-    DetectorPosition{"clover_B1_CUp_1", 135. * degree, 0. * degree, 8. * inch, 90. * degree, filterCuThickThickness, 2 * filterCuThinThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"clover_B2_CUp_2", 125.26 * degree, 45. * degree, 8. * inch, 0. * degree, filterCuThickThickness, filterCuThickThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"clover_B4_CUp_3", 125.26 * degree, 135. * degree, 8. * inch, 0. * degree, filterCuThickThickness, filterCuThickThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"clover_B5_CUp_4", 135. * degree, 180. * degree, 8. * inch, 270. * degree, filterCuThickThickness, filterCuThickThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"clover_B6_CDown_1", 125.26 * degree, 225. * degree, 8. * inch, 90. * degree, filterCuThickThickness, 2 * filterCuThinThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"clover_B8_CDown_2", 125.26 * degree, 315. * degree, 8. * inch, 180. * degree, filterCuThickThickness, filterCuThickThickness, G4ThreeVector(0, 0, 0)},
+
+array<DetectorPosition, 5> clover_positions{
+    DetectorPosition{"clover_B1_CUp_1", 90. * degree, 0. * degree, 13.0 * cm, 270. * degree, filterCuThickThickness, 2 * filterCuThinThickness, G4ThreeVector(0, 0, 0)},
+    DetectorPosition{"clover_B2_CUp_2", 90. * degree, 90. * degree, 13.0 * cm, 270. * degree, filterCuThickThickness, filterCuThickThickness, G4ThreeVector(0, 0, 0)},
+    DetectorPosition{"clover_B4_CUp_3", 125.26 * degree, 45. * degree, 12. * cm, 90. * degree, filterCuThickThickness, filterCuThickThickness, G4ThreeVector(0, 0, 0)},
+    DetectorPosition{"clover_B5_CUp_4", 135. * degree, 90. * degree, 21. * cm, 90. * degree, filterCuThickThickness, filterCuThickThickness, G4ThreeVector(0, 0, 0)},
+    DetectorPosition{"clover_B6_CDown_1", 125.26 * degree, 135. * degree, 10.5 * cm, 0. * degree, filterCuThickThickness, 2 * filterCuThinThickness, G4ThreeVector(0, 0, 0)},
+    //DetectorPosition{"clover_B8_CDown_2", 125.26 * degree, 315. * degree, 8. * inch, 180. * degree, filterCuThickThickness, filterCuThickThickness, G4ThreeVector(0, 0, 0)},
 };
 
-array<DetectorPosition, 10> cebr_positions{
-    DetectorPosition{"cebr_B_Sci_05", 90. * degree, 27.5 * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"cebr_C_Sci_06", 90. * degree, 45. * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"cebr_D_Sci_07", 90. * degree, 62.5 * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"cebr_F_Sci_08", 90. * degree, 117.5 * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"cebr_G_Sci_09", 90. * degree, 135. * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"cebr_H_Sci_10", 90. * degree, 152.5 * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"cebr_K_Sci_11", 90. * degree, 225. * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"cebr_O_Sci_12", 90. * degree, 315. * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"cebr_BD_Sci_13", 135. * degree, 90. * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
-    DetectorPosition{"cebr_BK_Sci_14", 135. * degree, 270. * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
+array<DetectorPosition, 3> cebr_positions{
+   // DetectorPosition{"cebr_B_Sci_05", 90. * degree, 27.5 * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
+    DetectorPosition{"cebr_C_Sci_06", 90. * degree, 45. * degree, 7.5 * cm, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
+   // DetectorPosition{"cebr_D_Sci_07", 90. * degree, 62.5 * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
+   // DetectorPosition{"cebr_F_Sci_08", 90. * degree, 117.5 * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
+    DetectorPosition{"cebr_G_Sci_09", 90. * degree, 135. * degree, 8.5 * cm, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
+  //  DetectorPosition{"cebr_H_Sci_10", 90. * degree, 152.5 * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
+   // DetectorPosition{"cebr_K_Sci_11", 90. * degree, 225. * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
+    DetectorPosition{"cebr_O_Sci_12", 90. * degree, 315. * degree, 10.5 * cm, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
+   // DetectorPosition{"cebr_BD_Sci_13", 135. * degree, 90. * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
+   // DetectorPosition{"cebr_BK_Sci_14", 135. * degree, 270. * degree, 8. * inch, 0., filterCuThinThickness, filterPbThinThickness, G4ThreeVector(0, 0, 0)},
 };
 
 DetectorConstruction::DetectorConstruction() {}
